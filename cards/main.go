a0883c5e9f75c05b3dae100199aec59aa6d16c0d
Jason McCutchan <jason.mccutchan@gmail.com>
2018-11-08 17:33:47 -0500

    new deck instance creation func and some population with nested loops

--- cards/main.go
+++ cards/main.go
@@ -7,13 +7,6 @@ import (
 var card string
 
 func main() {
-	cards := []string{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
-}
-
-func newCard() string {
-	return "Five of Diamonds"
+	cards := newDeck()
+	fmt.Println(cards)
 }
